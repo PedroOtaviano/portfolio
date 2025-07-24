2ac9d0edb4f4af08307566d3192b607023c7fc69
PedroOtaviano <pedro.redes23@gmail.com>
2025-07-23 20:39:53 -0400

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,18 @@
 import Titulo from "../../components/Título"
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
+
+import { BotaoTema, Descricao, SidebarContainer } from "./styles"
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/PedroOtaviano.png" alt="" />
-    <Titulo fontSize={20}>Pedro Otaviano</Titulo>
+    <SidebarContainer>
+      <Avatar/>
+      <Titulo fontSize={20}>Pedro Otaviano</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>pedrootaviano</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>Engenheiro full-stack</Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
